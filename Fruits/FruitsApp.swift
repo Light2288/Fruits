688165ae20d0e55094182a29ab6da8ddf3755a1f
Light2288 <davide.aliti@gmail.com>
2022-04-10 23:09:15 +0200

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Davide Aliti on 10/04/22.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
